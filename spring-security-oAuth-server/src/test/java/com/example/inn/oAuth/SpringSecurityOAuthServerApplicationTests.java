b2b1f7ef3a6ac368009af5f7d7a8610d84d9cbed
sayantani <sayantaniit1997@gmail.com>
2021-04-21 14:56:41 +0530

    fccc

---
+++ spring-security-oAuth-server/src/test/java/com/example/inn/oAuth/SpringSecurityOAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inn.oAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
