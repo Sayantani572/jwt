8224fd9172ebe53581fe9defbbfb8d6b58d58606
sayantani <sayantaniit1997@gmail.com>
2021-04-21 14:54:10 +0530

    fc

---
+++ spring-security-oAuth-client/src/main/java/com/example/inn/oAuth/client/SpringSecurityOAuthClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.inn.oAuth.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOAuthClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOAuthClientApplication.class, args);
+	}
+
+}
